5f70e779f6d86cc14cc5bb0a66d7eecdde3d97f3
hj <jjjjhjjjj123@gmail.com>
2020-12-17 00:33:36 +0900

    repacktoring getting youtube list and set environment key

--- src/app.jsx
+++ src/app.jsx
@@ -3,45 +3,22 @@ import "./app.css";
 import VideoList from "./components/video_list/video_list";
 import SearchHeader from "./components/search_header/search_header";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   // const [search, setSearch] = useState();
 
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=26&key=AIzaSyBlk9XU9tEPFtzLY9_D5wVNxF-V-u5_-DI",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
+    youtube
+      .mostPopular() //
+      .then((result) => setVideos(result))
       .catch((error) => console.log("error", error));
   }, []);
 
   const handleSearch = (search) => {
     // setSearch(search);
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=26&key=AIzaSyBlk9XU9tEPFtzLY9_D5wVNxF-V-u5_-DI&q=${search}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => {
-        const data = result.items.map((item) => ({
-          ...item,
-          id: item.id.videoId,
-        }));
-
-        setVideos(data);
-      })
+    youtube
+      .search(search) //
+      .then((response) => setVideos(response))
       .catch((error) => console.log("error", error));
   };
 
---
+++ src/service/youtube.js
@@ -0,0 +1,37 @@
+class Youtube {
+  constructor(key) {
+    this.key = key;
+
+    this.getRequestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    this.link = "https://youtube.googleapis.com/youtube/v3";
+  }
+
+  mostPopular() {
+    return fetch(
+      `${this.link}/videos?part=snippet&chart=mostPopular&maxResults=26&key=${this.key}`,
+      this.requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => result.items);
+  }
+
+  search(search) {
+    return fetch(
+      `${this.link}/search?part=snippet&maxResults=26&key=${this.key}&q=${search}`,
+      this.requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({
+          ...item,
+          id: item.id.videoId,
+        }))
+      );
+  }
+}
+
+export default Youtube;
